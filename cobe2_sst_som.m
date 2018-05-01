060378707438150e7be44f30f8c60647a80622cf
'mpdannenberg' <'dannenberg@email.arizona.edu'>
2018-05-01 11:59:51 -0700

    Get rid of old unnecessary stuff

--- cobe2_sst_som.m
+++ cobe2_sst_som.m
@@ -68,6 +68,8 @@ lon0 = lon(lon>=min(lonlim) & lon<=max(lonlim));
 % end
 % clear numNodes comb maxNodes;
 
+% Some issues here? Should be monotonic
+
 %% Fit final SOM
 % Train SOM
 n = 12;
@@ -87,14 +89,10 @@ clr = [103,0,31
     67,147,195
     33,102,172
     5,48,97]/255;
-% ax = tight_subplot(n,1,0,0.1,0.1);
 ax = tight_subplot(6,2,0,[0.15 0.05],0.1);
 
 
 for i = 1:n
-%     temp = NaN(1, length(lat0)*length(lon0));
-%     temp(idx==nt) = sM.codebook(i, :);
-%     temp = reshape(temp, length(lat0), length(lon0));
 
     temp = squeeze(nanmean(ssta(lat>=min(latlim) & lat<=max(latlim), lon>=min(lonlim) & lon<=max(lonlim),Bmus==i), 3));
     
@@ -133,70 +131,4 @@ xlabel(cb, ['SST anomaly (',sprintf('%c', char(176)),'C)'], 'FontSize',12);
 set(gcf,'PaperPositionMode','auto')
 print('-dtiff','-f1','-r600','./output/sst-som-12nodes.tif')
 close all;
-
-
-
-
-states = shaperead('usastatehi','UseGeoCoords', true);
-worldland = shaperead('landareas','UseGeoCoords', true);
-latlim = [-28 28];
-lonlim = [120 300];
-clr = [103,0,31
-178,24,43
-214,96,77
-244,165,130
-253,219,199
-209,229,240
-146,197,222
-67,147,195
-33,102,172
-5,48,97]/255;
-
-h = figure('Color','w');
-ax = tight_subplot(3,1,0,0.1,0.1);
-
-axes(ax(1))
-axesm('mercator','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',10,'MLineLocation',30,'MeridianLabel','on',...
-        'ParallelLabel','on','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        [0.7 0.7 0.7], 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',27.99,...
-        'FEdgeColor','w');
-axis off;
-axis image;
-surfm(lat, lon, squeeze(sst(:,:,yr==1997 & mo==12)));
-caxis([21 35]);
-colormap(gca, [254,240,217
-    253,212,158
-    253,187,132
-    252,141,89
-    239,101,72
-    215,48,31
-    153,0,0]/255);
-
-axes(ax(2))
-axesm('mercator','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',10,'MLineLocation',30,'MeridianLabel','off',...
-        'ParallelLabel','on','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        [0.7 0.7 0.7], 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',27.99,...
-        'FEdgeColor','w');
-axis off;
-axis image;
-surfm(lat, lon, squeeze(ssta(:,:,yr==1997 & mo==12)));
-caxis([-3 3]);
-colormap(gca, flipud(clr));
-
-axes(ax(3))
-axesm('mercator','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',10,'MLineLocation',30,'MeridianLabel','off',...
-        'ParallelLabel','on','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        [0.7 0.7 0.7], 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',27.99,...
-        'FEdgeColor','w');
-axis off;
-axis image;
-surfm(lat, lon, squeeze(sstz(:,:,yr==1997 & mo==12)));
-caxis([-3 3]);
-colormap(gca, flipud(clr));
 
