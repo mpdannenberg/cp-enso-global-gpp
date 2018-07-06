0bd1a9adb3887c6423ace4373328bc7386d6c79d
'mpdannenberg' <'dannenberg@email.arizona.edu'>
2018-07-06 15:46:43 -0700

    Standardize and make figure

--- cobe2_sst_cp_ep.m
+++ cobe2_sst_cp_ep.m
@@ -10,7 +10,7 @@ windowSize = 6; % Number of months over which to average SSTs
 endMonth = 2; % End month of integration period
 
 %% Load COBE-2 SST data
-info = ncinfo('./data/sst.mon.mean.nc');
+% info = ncinfo('./data/sst.mon.mean.nc');
 lat = double(ncread('./data/sst.mon.mean.nc','lat'));
 lon = double(ncread('./data/sst.mon.mean.nc','lon'));
 t = ncread('./data/sst.mon.mean.nc','time');
@@ -30,7 +30,7 @@ for i = 1:12
     ssta(:,:,mo==i) = x - repmat(xm, 1,1,size(x, 3));
     
 end
-clear x xclim xm i;
+clear x xclim xm i sst;
 
 %% Filter to 6-month mean SST
 b = ones(1,windowSize)/windowSize;
@@ -43,7 +43,7 @@ idx = yr >= syear & mo == endMonth;
 ssta = ssta(:, :, idx);
 yr = yr(idx);
 lat_weight = repmat(sqrt(cosd(lat)), 1, length(lon));
-clear idx mo;
+clear idx mo endMonth;
 
 %% Regress SST data onto Nino-4 and Nino-1+2 indices to get independent CP- and EP- SST anomalies
 nino4 = ssta(lat>=-5 & lat<=5, lon>=160 & lon<=210, :);
@@ -77,7 +77,7 @@ for i = 1:size(sst_tp, 2)
     lm_cp = fitlm(nino12, y);
     sst_cp(:, i) = lm_cp.Residuals.Raw;
 end
-clear y lm_ep lm_cp i;
+clear y lm_ep lm_cp i sst_tp;
 
 [ep_coef, ep_pc, ep_latent] = pca(sst_ep, 'VariableWeights',w_tp);
 [cp_coef, cp_pc, cp_latent] = pca(sst_cp, 'VariableWeights',w_tp);
@@ -89,4 +89,94 @@ ep_eof = reshape(ep_eof, ny, nx);
 cp_eof = reshape(cp_eof, ny, nx);
 ep_idx = ep_pc(:, 1);
 cp_idx = cp_pc(:, 1);
+clear sst_idx w_tp ny nx;
 
+%% Get spatial patterns by correlating SSTa against CPI and EPI
+[ny, nx, nt] = size(ssta);
+cpi_r = NaN(ny, nx);
+epi_r = NaN(ny, nx);
+for i = 1:ny
+    for j = 1:nx
+        y = squeeze(ssta(i, j, :));
+        cpi_r(i,j) = corr(y, cp_idx);
+        epi_r(i,j) = corr(y, ep_idx);
+    end
+end
+clear i j y;
+
+%% Standardize CPI and EPI
+cpi = (cp_idx - mean(cp_idx(yr>=1981 & yr<=2010))) / std(cp_idx(yr>=1981 & yr<=2010));
+epi = (ep_idx - mean(ep_idx(yr>=1981 & yr<=2010))) / std(ep_idx(yr>=1981 & yr<=2010));
+clear cp_coef cp_eof cp_idx cp_latent cp_pc ep_coef ep_eof ep_idx ep_latent ep_pc;
+
+%% Figure
+h = figure('Color','w');
+h.Units = 'inches';
+h.Position = [1 1 7 6.4];
+
+clr = [103,0,31
+178,24,43
+214,96,77
+244,165,130
+253,219,199
+209,229,240
+146,197,222
+67,147,195
+33,102,172
+5,48,97]/255;
+
+latlim = [-25 25];
+
+ax = tight_subplot(3,1,0,0.1,[0.06 0.14]);
+
+axes(ax(1))
+axesm('mercator','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
+            'on','PLineLocation',10,'MLineLocation',30,'MeridianLabel','on',...
+            'ParallelLabel','on','GLineWidth',0.5,'Frame','on','FFaceColor',...
+            [0.7 0.7 0.7], 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
+            'FLineWidth',1, 'FontColor',[0.2 0.2 0.2], 'MLabelParallel',25,...
+            'FEdgeColor','w','FontSize',7);
+axis off;
+axis image;
+contourm(lat, lon, epi_r, 'Fill','on', 'LevelList',-1:0.2:1);
+colormap(gca, flipud(clr));
+caxis([-1 1]);
+textm(20,125,'A', 'FontSize',14);
+
+axes(ax(2))
+axesm('mercator','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
+            'on','PLineLocation',10,'MLineLocation',30,'MeridianLabel','off',...
+            'ParallelLabel','on','GLineWidth',0.5,'Frame','on','FFaceColor',...
+            [0.7 0.7 0.7], 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
+            'FLineWidth',1, 'FontColor',[0.2 0.2 0.2], 'MLabelParallel',25,...
+            'FEdgeColor','w','FontSize',7);
+axis off;
+axis image;
+contourm(lat, lon, cpi_r, 'Fill','on', 'LevelList',-1:0.2:1);
+colormap(gca, flipud(clr));
+caxis([-1 1]);
+textm(20,125,'B', 'FontSize',14);
+
+cb = colorbar('eastoutside');
+cb.Position = [0.87 0.367 0.04 0.535];
+cb.TickLength = 0.08;
+xlabel(cb, 'Pearson''s R', 'FontSize',11);
+
+axes(ax(3))
+plot(yr, epi, '-', 'Color',[178,24,43]/255, 'LineWidth',2);
+hold on;
+plot(yr, cpi, '-', 'Color',[33,102,172]/255, 'LineWidth',2);
+set(gca, 'YLim',[-3.5 3.5],'YTick',-3:3, 'XLim',[1950 2016], 'TickLength',[0 0],...
+    'XColor',[0.2 0.2 0.2], 'YColor',[0.2 0.2 0.2])
+grid on;
+hold off;
+xlabel('Year')
+text(1952,2.5, 'C', 'FontSize',14)
+lgd = legend('EPI','CPI','Location','southwest');
+legend('boxoff');
+lgd.FontSize = 12;
+lgd.Position = [0.0794 0.1 0.1161 0.0757];
+
+set(gcf,'PaperPositionMode','auto')
+print('-dtiff','-f1','-r300','./output/epi-cpi-1951-2016.tif')
+close all;
