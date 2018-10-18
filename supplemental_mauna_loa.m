6f8a9d433b3a4507d686ca1e94bbbaa3b41e3fb9
'mpdannenberg' <'dannenberg@email.arizona.edu'>
2018-10-18 13:38:01 -0500

    Add subplot labels

--- supplemental_mauna_loa.m
+++ supplemental_mauna_loa.m
@@ -75,50 +75,50 @@ axes(ha(1))
 plot(epi, gr, 'ko', 'LineWidth',1.5);
 set(gca, 'XLim',[-3.5 3.5], 'YLim',[-1.75 1.75],'XTickLabels','', 'TickLength',[0 0]);
 ylabel({'CO_{2} growth rate anomaly (ppm)'},'FontSize',9)
-%text(1961, 1.5, 'A', 'FontSize',12)
 grid on;
 [r,p] = corr(epi, gr);
-text(-3,1.4,['R = ',num2str(round(r,2))],'FontSize',10)
-text(-3,1.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(-3,-1.1,['R = ',num2str(round(r,2))],'FontSize',10)
+text(-3,-1.4,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
 mdl = fitlm(epi, gr);
 hold on;
 plot([min(epi) max(epi)],...
     mdl.Coefficients.Estimate(2)*[min(epi) max(epi)]+mdl.Coefficients.Estimate(1),...
     '-', 'Color',[0.4 0.4 0.4], 'LineWidth',2)
 hold off;
+text(-2.9, 1.4, 'A', 'FontSize',14);
 
 axes(ha(2))
 plot(cpi, gr, 'ko', 'LineWidth',1.5);
 set(gca, 'XLim',[-3.5 3.5], 'YLim',[-1.75 1.75],'XTickLabels','',...
     'TickLength',[0 0],'YAxisLocation','right');
 ylabel({'CO_{2} growth rate anomaly (ppm)'},'FontSize',9)
-% text(1961, 1.5, 'A', 'FontSize',12)
 grid on;
 [r,p] = corr(cpi, gr);
 text(2,1.4,['R = ',num2str(round(r,2))],'FontSize',10)
 text(2,1.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(-2.9, 1.4, 'B', 'FontSize',14);
 
 axes(ha(3))
 plot(epi, am, 'ko', 'LineWidth',1.5);
 set(gca, 'XLim',[-3.5 3.5], 'YLim',[4.25 7.75],'TickLength',[0 0]);
 ylabel({'CO_{2} seasonal amplitude (ppm)'},'FontSize',9)
-%text(1961, 1.5, 'A', 'FontSize',12)
 grid on;
 xlabel('Eastern Pacific ENSO index','FontSize',9)
 [r,p] = corr(epi, am);
-text(-3,7.4,['R = ',num2str(round(r,2))],'FontSize',10)
-text(-3,7.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(2,7.4,['R = ',num2str(round(r,2))],'FontSize',10)
+text(2,7.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(-2.9, 7.4, 'C', 'FontSize',14);
 
 axes(ha(4))
 plot(cpi, am, 'ko', 'LineWidth',1.5);
 set(gca, 'XLim',[-3.5 3.5], 'YLim',[4.25 7.75],'TickLength',[0 0],'YAxisLocation','right');
 ylabel({'CO_{2} seasonal amplitude (ppm)'},'FontSize',9)
-% text(1961, 1.5, 'A', 'FontSize',12)
 grid on;
 xlabel('Central Pacific ENSO index','FontSize',9)
 [r,p] = corr(cpi, am);
-text(-3,7.4,['R = ',num2str(round(r,2))],'FontSize',10)
-text(-3,7.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(2,7.4,['R = ',num2str(round(r,2))],'FontSize',10)
+text(2,7.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+text(-2.9, 7.4, 'D', 'FontSize',14);
 
 set(gcf,'PaperPositionMode','auto')
 print('-dtiff','-f1','-r300','./output/supplemental-mauna-loa-scatterplot.tif')
