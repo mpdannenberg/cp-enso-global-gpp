2c75fd1c1929693fa44825a766b53ee9c2e0aff0
mpdannenberg <matthew-dannenberg@uiowa.edu>
2019-07-15 16:12:35 -0500

    Change to comparison of three strongest EP and CP La Niñas

--- make_cp_ep_gpp_maps_byYear.m
+++ make_cp_ep_gpp_maps_byYear.m
@@ -1,4 +1,4 @@
-% Make some maps and stuff
+% Make maps of three strongest EP and CP La Niña years
 
 clr = [84,48,5
 140,81,10
@@ -17,14 +17,14 @@ worldland = shaperead('landareas','UseGeoCoords', true);
 
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 -1 7 9];
+h.Position = [1 1 7 7];
 
 load('./data/cp_ep_gpp_lue.mat');
 
 GPP_annual_mean(GPP_annual_mean==0) = NaN;
 
-%% Eastern Pacific El Niños
-subplot(5,3,1)
+%% Eastern Pacific La Niñas
+subplot(3,2,1)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -32,19 +32,19 @@ ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
 axis off;
 axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1983)))
+surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1991)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
 pos = get(gca, 'Position');
 pos(1) = pos(1)-0.04;
 set(gca, 'Position',pos);
-text(-0.55,-0.85,'1983', 'FontSize',14, 'FontWeight','bold');
-ttl = title('Eastern Pacific El Niños', 'FontSize',12);
-ttl.Position(2) = 2;
+textm(-60,0,'1991', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
+ttl = title('Eastern Pacific La Niñas', 'FontSize',12);
+ttl.Position(2) = 1.8;
 subplotsqueeze(gca,1.3);
 
-subplot(5,3,4)
+subplot(3,2,3)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -52,65 +52,17 @@ ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
 axis off;
 axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1998)))
+surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2002)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
 pos = get(gca, 'Position');
 pos(1) = pos(1)-0.04;
 set(gca, 'Position',pos);
-text(-0.55,-0.85,'1998', 'FontSize',14, 'FontWeight','bold');
+textm(-60,0,'2002', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
 subplotsqueeze(gca,1.3);
 
-%% Central Pacific El Niños
-subplot(5,3,2)
-ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
-        'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        'none', 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
-axis off;
-axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1992)))
-caxis([-0.2 0.2])
-colormap(gca, clr);
-geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-text(-0.55,-0.85,'1992', 'FontSize',14, 'FontWeight','bold');
-ttl = title('Central Pacific El Niños', 'FontSize',12);
-ttl.Position(2) = 2;
-subplotsqueeze(gca,1.3);
-
-subplot(5,3,5)
-ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
-        'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        'none', 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
-axis off;
-axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1995)))
-caxis([-0.2 0.2])
-colormap(gca, clr);
-geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-text(-0.55,-0.85,'1995', 'FontSize',14, 'FontWeight','bold');
-subplotsqueeze(gca,1.3);
-
-subplot(5,3,8)
-ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
-        'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        'none', 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
-axis off;
-axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2003)))
-caxis([-0.2 0.2])
-colormap(gca, clr);
-geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-text(-0.55,-0.85,'2003', 'FontSize',14, 'FontWeight','bold');
-subplotsqueeze(gca,1.3);
-
-subplot(5,3,11)
+subplot(3,2,5)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -122,46 +74,14 @@ surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2005)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-text(-0.55,-0.85,'2005', 'FontSize',14, 'FontWeight','bold');
-subplotsqueeze(gca,1.3);
-
-subplot(5,3,14)
-ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
-        'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        'none', 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
-axis off;
-axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2010)))
-caxis([-0.2 0.2])
-colormap(gca, clr);
-geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-text(-0.55,-0.85,'2010', 'FontSize',14, 'FontWeight','bold');
-subplotsqueeze(gca,1.3);
-
-%% Mixed El Niños
-subplot(5,3,3)
-ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
-        'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
-        'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
-        'none', 'FontName', 'Helvetica','GColor',[0.6 0.6 0.6],...
-        'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
-axis off;
-axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1987)))
-caxis([-0.2 0.2])
-colormap(gca, clr);
-geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
 pos = get(gca, 'Position');
-pos(1) = pos(1)+0.04;
+pos(1) = pos(1)-0.04;
 set(gca, 'Position',pos);
-text(-0.55,-0.85,'1987', 'FontSize',14, 'FontWeight','bold');
-ttl = title('Mixed El Niños', 'FontSize',12);
-ttl.Position(2) = 2;
+textm(-60,0,'2005', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
 subplotsqueeze(gca,1.3);
 
-subplot(5,3,6)
+%% Central Pacific El Niños
+subplot(3,2,2)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -169,17 +89,16 @@ ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
 axis off;
 axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1988)))
+surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1989)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-pos = get(gca, 'Position');
-pos(1) = pos(1)+0.04;
-set(gca, 'Position',pos);
-text(-0.55,-0.85,'1988', 'FontSize',14, 'FontWeight','bold');
+textm(-60,0,'1989', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
+ttl = title('Central Pacific La Niñas', 'FontSize',12);
+ttl.Position(2) = 1.8;
 subplotsqueeze(gca,1.3);
 
-subplot(5,3,9)
+subplot(3,2,4)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -187,17 +106,14 @@ ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
 axis off;
 axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2007)))
+surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==1999)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-pos = get(gca, 'Position');
-pos(1) = pos(1)+0.04;
-set(gca, 'Position',pos);
-text(-0.55,-0.85,'2007', 'FontSize',14, 'FontWeight','bold');
+textm(-60,0,'1999', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
 subplotsqueeze(gca,1.3);
 
-subplot(5,3,12)
+subplot(3,2,6)
 ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'on','PLineLocation',30,'MLineLocation',60,'MeridianLabel','off',...
         'ParallelLabel','off','GLineWidth',0.5,'Frame','on','FFaceColor',...
@@ -205,26 +121,24 @@ ax = axesm('winkel','MapLatLimit',latlim,'MapLonLimit',lonlim,'grid',...
         'FLineWidth',1, 'FontColor',[0.5 0.5 0.5], 'MLabelParallel',min(latlim)+0.11);
 axis off;
 axis image;
-surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2016)))
+surfm(lat, lon, squeeze(GPP_annual_mean(:,:,years==2011)))
 caxis([-0.2 0.2])
 colormap(gca, clr);
 geoshow(worldland,'FaceColor','none','EdgeColor',[0.6 0.6 0.6])
-pos = get(gca, 'Position');
-pos(1) = pos(1)+0.04;
-set(gca, 'Position',pos);
-text(-0.55,-0.85,'2016', 'FontSize',14, 'FontWeight','bold');
+textm(-60,0,'2011', 'FontSize',14, 'FontWeight','bold', 'HorizontalAlignment','center', 'VerticalAlignment','bottom');
 subplotsqueeze(gca,1.3);
+
 
 
 %% Color bar
 cb = colorbar('southoutside');
-cb.Position = [0.06    0.51    0.28    0.03];
+cb.Position = [0.2    0.06    0.6    0.03];
 cb.Ticks = -0.2:0.04:0.2;
-cb.TickLength = 0.13;
+cb.TickLength = 0.05;
 cb.TickLabels = {'-200','','','','','0','','','','','200'};
 ylb = ylabel(cb, 'GPP anomaly (g C m^{-2} yr^{-1})');
 
 set(gcf,'PaperPositionMode','auto')
-print('-dtiff','-f1','-r300','./output/supplemental-ep-cp-mixed-gpp-anomalies.tif')
+print('-dtiff','-f1','-r300','./output/supplemental-ep-cp-LaNina-gpp-anomalies.tif')
 close all;
 
