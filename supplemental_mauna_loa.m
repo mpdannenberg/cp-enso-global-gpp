7294d37b6e4120b9f46b9324295cc06bd0e5d2a3
mpdannenberg <matthew-dannenberg@uiowa.edu>
2020-09-24 16:30:07 -0500

    Add CO2 amplitude

--- supplemental_mauna_loa.m
+++ supplemental_mauna_loa.m
@@ -27,29 +27,44 @@ epi = epi(idx); cpi = cpi(idx); yr = yr(idx); clear idx;
 %% Plot time series
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 1 7 4];
+h.Position = [1 1 7 6];
 
-ha = tight_subplot(3, 1, 0, [0.1 0.1], [0.1 0.1]);
+ha = tight_subplot(4, 1, 0, [0.1 0.1], [0.1 0.1]);
 
 axes(ha(1))
 plot(yr, gr, 'k-', 'LineWidth',1.5);
-set(gca, 'XLim',[min(yr) max(yr)], 'XTickLabels','','XColor','w', 'TickDir','out','TickLength',[0.01 0.02]);
+set(gca, 'XLim',[min(yr) max(yr)], 'XTickLabels','','XColor','w',...
+    'TickDir','out','TickLength',[0.01 0.02], 'YAxisLocation','right');
 ylabel({'CO_{2} growth rate', 'anomaly (ppm)'},'FontSize',8)
-text(1961, 1.5, 'A', 'FontSize',12)
+ylim = get(gca, 'YLim');
+text(1961, ylim(2)-0.1*diff(ylim), 'A', 'FontSize',12)
 box off;
 
 axes(ha(2))
-plot(yr, epi, 'k-', 'LineWidth',1.5);
-set(gca, 'XLim',[min(yr) max(yr)], 'YAxisLocation','right', 'XTickLabels','', 'YLim',[-3 3],'XColor','w', 'TickDir','out','TickLength',[0.01 0.02]);
-ylabel({'Eastern Pacific', 'ENSO index'},'FontSize',8)
-text(1961, 2.25, 'B', 'FontSize',12)
+plot(yr, am, 'k-', 'LineWidth',1.5);
+set(gca, 'XLim',[min(yr) max(yr)], 'XTickLabels','','XColor','w',...
+    'TickDir','out','TickLength',[0.01 0.02]);
+ylabel({'CO_{2} amplitude (ppm)'},'FontSize',8)
+ylim = get(gca, 'YLim');
+text(1961, ylim(2)-0.1*diff(ylim), 'B', 'FontSize',12)
 box off;
 
 axes(ha(3))
+plot(yr, epi, 'k-', 'LineWidth',1.5);
+set(gca, 'XLim',[min(yr) max(yr)], 'XTickLabels','', 'YLim',[-1.2 3.2],...
+    'XColor','w', 'TickDir','out','TickLength',[0.01 0.02], 'YAxisLocation','right');
+ylabel({'EP anomaly (K)'},'FontSize',8)
+ylim = get(gca, 'YLim');
+text(1961, ylim(2)-0.1*diff(ylim), 'C', 'FontSize',12)
+box off;
+
+axes(ha(4))
 plot(yr, cpi, 'k-', 'LineWidth',1.5);
-set(gca, 'XLim',[min(yr) max(yr)], 'YLim',[-3 3], 'TickDir','out','TickLength',[0.01 0.02]);
-ylabel({'Central Pacific', 'ENSO index'},'FontSize',8)
-text(1961, 2.25, 'C', 'FontSize',12)
+set(gca, 'XLim',[min(yr) max(yr)], 'YLim',[-1.5 1.5], 'TickDir','out',...
+    'TickLength',[0.01 0.02]);
+ylabel({'CP anomaly (K)'},'FontSize',8)
+ylim = get(gca, 'YLim');
+text(1961, ylim(2)-0.1*diff(ylim), 'D', 'FontSize',12)
 box off;
 
 set(gcf,'PaperPositionMode','auto')
@@ -59,44 +74,88 @@ close all;
 %% Scatterplots
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 1 7 3];
+h.Position = [1 1 7 6];
 
-ha = tight_subplot(1, 2, 0, [0.15 0.05], [0.1 0.1]);
+ha = tight_subplot(2, 2, 0, [0.15 0.05], [0.1 0.1]);
 
 % Growth rate anomalies
 axes(ha(1))
 plot(epi, gr, 'ko', 'LineWidth',1.5);
-set(gca, 'XLim',[-3.5 3.5], 'YLim',[-1.75 1.75],'TickLength',[0 0]);
+set(gca, 'XLim',[-1.5 3.5], 'YLim',[-1.75 1.75],'TickLength',[0 0],...
+    'XTickLabel','');
 ylabel({'CO_{2} growth rate anomaly (ppm)'},'FontSize',9)
 grid on;
 [r,p] = corr(epi, gr);
-text(-3,-1.1,['R = ',num2str(round(r,2))],'FontSize',10)
-text(-3,-1.4,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
 mdl = fitlm(epi, gr);
 hold on;
 plot([min(epi) max(epi)],...
     mdl.Coefficients.Estimate(2)*[min(epi) max(epi)]+mdl.Coefficients.Estimate(1),...
     '-', 'Color',[0.4 0.4 0.4], 'LineWidth',2)
 hold off;
-text(-2.9, 1.4, 'A', 'FontSize',14);
-xlabel('Eastern Pacific ENSO index','FontSize',9)
+ylim = get(gca, 'YLim');
+xlim = get(gca, 'XLim');
+text(xlim(1)+0.05*diff(xlim), ylim(2)-0.05*diff(ylim), 'A', 'FontSize',14);
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.05*diff(ylim),['R = ',num2str(round(r,2))],'FontSize',10)
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.1*diff(ylim),['{\itp} = ',num2str(round(p,2))],'FontSize',10)
 
 axes(ha(2))
 plot(cpi, gr, 'ko', 'LineWidth',1.5);
-set(gca, 'XLim',[-3.5 3.5], 'YLim',[-1.75 1.75],'TickLength',[0 0],'YAxisLocation','right');
+set(gca, 'XLim',[-1.5 1.5], 'YLim',[-1.75 1.75],'TickLength',[0 0],...
+    'YAxisLocation','right', 'XTickLabel','');
 ylabel({'CO_{2} growth rate anomaly (ppm)'},'FontSize',9)
 grid on;
 [r,p] = corr(cpi, gr);
-text(2,1.4,['R = ',num2str(round(r,2))],'FontSize',10)
-text(2,1.1,['{\itp} = ',num2str(round(p,2))],'FontSize',10)
 mdl = fitlm(cpi, gr);
 hold on;
 plot([min(cpi) max(cpi)],...
     mdl.Coefficients.Estimate(2)*[min(cpi) max(cpi)]+mdl.Coefficients.Estimate(1),...
     '-', 'Color',[0.4 0.4 0.4], 'LineWidth',2)
 hold off;
-text(-2.9, 1.4, 'B', 'FontSize',14);
-xlabel('Central Pacific ENSO index','FontSize',9)
+ylim = get(gca, 'YLim');
+xlim = get(gca, 'XLim');
+text(xlim(1)+0.05*diff(xlim), ylim(2)-0.05*diff(ylim), 'B', 'FontSize',14);
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.05*diff(ylim),['R = ',num2str(round(r,2))],'FontSize',10)
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.1*diff(ylim),['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+
+% Amplitude
+axes(ha(3))
+plot(epi, am, 'ko', 'LineWidth',1.5);
+set(gca, 'XLim',[-1.5 3.5], 'YLim',[4.5 7],'TickLength',[0 0]);
+ylabel({'Annual CO_{2} amplitude (ppm)'},'FontSize',9)
+grid on;
+[r,p] = corr(epi, am);
+mdl = fitlm(epi, am);
+hold on;
+plot([min(epi) max(epi)],...
+    mdl.Coefficients.Estimate(2)*[min(epi) max(epi)]+mdl.Coefficients.Estimate(1),...
+    '-', 'Color',[0.4 0.4 0.4], 'LineWidth',2)
+hold off;
+ylim = get(gca, 'YLim');
+xlim = get(gca, 'XLim');
+text(xlim(1)+0.05*diff(xlim), ylim(2)-0.05*diff(ylim), 'C', 'FontSize',14);
+text(xlim(1)+0.7*diff(xlim), ylim(2)-0.05*diff(ylim),['R = ',num2str(round(r,2))],'FontSize',10)
+text(xlim(1)+0.7*diff(xlim), ylim(2)-0.1*diff(ylim),['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+xlabel('EP anomaly (K)','FontSize',9)
+
+axes(ha(4))
+plot(cpi, am, 'ko', 'LineWidth',1.5);
+set(gca, 'XLim',[-1.5 1.5], 'YLim',[4.5 7],'TickLength',[0 0],...
+    'YAxisLocation','right');
+ylabel({'Annual CO_{2} amplitude (ppm)'},'FontSize',9)
+grid on;
+[r,p] = corr(cpi, am);
+mdl = fitlm(cpi, am);
+hold on;
+plot([min(cpi) max(cpi)],...
+    mdl.Coefficients.Estimate(2)*[min(cpi) max(cpi)]+mdl.Coefficients.Estimate(1),...
+    '-', 'Color',[0.4 0.4 0.4], 'LineWidth',2)
+hold off;
+ylim = get(gca, 'YLim');
+xlim = get(gca, 'XLim');
+text(xlim(1)+0.05*diff(xlim), ylim(2)-0.05*diff(ylim), 'D', 'FontSize',14);
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.05*diff(ylim),['R = ',num2str(round(r,2))],'FontSize',10)
+text(xlim(1)+0.2*diff(xlim), ylim(2)-0.1*diff(ylim),['{\itp} = ',num2str(round(p,2))],'FontSize',10)
+xlabel('CP anomaly (K)','FontSize',9)
 
 set(gcf,'PaperPositionMode','auto')
 print('-dtiff','-f1','-r300','./output/supplemental-mauna-loa-scatterplot.tif')
