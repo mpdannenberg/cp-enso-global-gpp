d710dcfc304e106dcea4828fc02ce59315b26643
mpdannenberg <matthew-dannenberg@uiowa.edu>
2020-10-13 10:03:36 -0500

    Initial commit

---
+++ commonPeriod/cp_ep_nep_inversions.m
@@ -0,0 +1,239 @@
+% Examine relationships between modeled MsTMIP GPP and EPI/CPI over 
+% 1951-2010 period
+syear = 1982;
+eyear = 2010;
+
+models = {'v18r2'}; nm = length(models);
+
+%% Regress monthly & annual gpp against CPI and EPI indices
+load ../data/nep_inversions.mat;
+load ../data/cpi_epi_1951-2016.mat;
+
+[ny,nx,~] = size(NEP_annual_mean);
+
+cpi = cpi(yr>=syear & yr<=eyear);
+epi = epi(yr>=syear & yr<=eyear);
+yr = yr(yr>=syear & yr<=eyear);
+
+NEP_annual_mean = NEP_annual_mean(:,:,years>=syear & years<=eyear);
+NEP_global_annual = NEP_global_annual(years>=syear & years<=eyear, :);
+NEP_global_annual_mean = NEP_global_annual_mean(years>=syear & years<=eyear, :);
+NEP_shyear_mean = NEP_shyear_mean(:,:,years>=syear & years<=eyear);
+NEP_global_shyear = NEP_global_shyear(years>=syear & years<=eyear, :);
+NEP_global_shyear_mean = NEP_global_shyear_mean(years>=syear & years<=eyear, :);
+NEP_global_monthly = NEP_global_monthly(years>=syear & years<=eyear, :, :);
+NEP_global_monthly_mean = NEP_global_monthly_mean(years>=syear & years<=eyear, :);
+years = years(years>=syear & years<=eyear)';
+
+EP_NEP_global_monthly_beta = NaN(12, length(models));
+EP_NEP_global_monthly_mean_beta = NaN(12, 1);
+EP_NEP_global_monthly_mean_beta_CI = NaN(12, 1);
+EP_NEP_global_annual_beta = NaN(1, length(models));
+EP_NEP_global_shyear_beta = NaN(1, length(models));
+
+mdl = fitlm(epi, NEP_global_annual_mean);
+EP_NEP_global_annual_mean_beta = mdl.Coefficients.Estimate(2);
+EP_NEP_global_annual_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Global inversion mean response to EPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+mdl = fitlm(epi, NEP_global_shyear_mean);
+EP_NEP_global_shyear_mean_beta = mdl.Coefficients.Estimate(2);
+EP_NEP_global_shyear_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Global inversion mean response to EPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+for i = 1:12
+    mdl = fitlm(epi, NEP_global_monthly_mean(:, i));
+    EP_NEP_global_monthly_mean_beta(i) = mdl.Coefficients.Estimate(2);
+    EP_NEP_global_monthly_mean_beta_CI(i) = 1.96*mdl.Coefficients.SE(2);
+    for j = 1:length(models)
+        mdl = fitlm(epi, NEP_global_monthly(:, i, j));
+        EP_NEP_global_monthly_beta(i, j) = mdl.Coefficients.Estimate(2);
+        if i == 1
+            mdl = fitlm(epi, NEP_global_annual(:, j));
+            EP_NEP_global_annual_beta(j) = mdl.Coefficients.Estimate(2);
+            mdl = fitlm(epi, NEP_global_shyear(:, j));
+            EP_NEP_global_shyear_beta(j) = mdl.Coefficients.Estimate(2);
+        end
+    end
+    
+end
+
+CP_NEP_global_monthly_beta = NaN(12, length(models));
+CP_NEP_global_monthly_mean_beta = NaN(12, 1);
+CP_NEP_global_monthly_mean_beta_CI = NaN(12, 1);
+CP_NEP_global_annual_beta = NaN(1, length(models));
+CP_NEP_global_shyear_beta = NaN(1, length(models));
+
+mdl = fitlm(cpi, NEP_global_annual_mean);
+CP_NEP_global_annual_mean_beta = mdl.Coefficients.Estimate(2);
+CP_NEP_global_annual_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Global inversion mean response to CPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+mdl = fitlm(cpi, NEP_global_shyear_mean);
+CP_NEP_global_shyear_mean_beta = mdl.Coefficients.Estimate(2);
+CP_NEP_global_shyear_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Global inversion mean response to CPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+for i = 1:12
+    mdl = fitlm(cpi, NEP_global_monthly_mean(:, i));
+    CP_NEP_global_monthly_mean_beta(i) = mdl.Coefficients.Estimate(2);
+    CP_NEP_global_monthly_mean_beta_CI(i) = 1.96*mdl.Coefficients.SE(2);
+    for j = 1:length(models)
+        mdl = fitlm(cpi, NEP_global_monthly(:, i, j));
+        CP_NEP_global_monthly_beta(i, j) = mdl.Coefficients.Estimate(2);
+        if i == 1
+            mdl = fitlm(cpi, NEP_global_annual(:, j));
+            CP_NEP_global_annual_beta(j) = mdl.Coefficients.Estimate(2);
+            mdl = fitlm(cpi, NEP_global_shyear(:, j));
+            CP_NEP_global_shyear_beta(j) = mdl.Coefficients.Estimate(2);
+        end
+    end
+    
+end
+
+clear i j mdl;
+
+%% Regress EPI and CPI vs. gridded NEP
+EP_NEP_annual_r = NaN(ny, nx);
+EP_NEP_annual_p = NaN(ny, nx);
+EP_NEP_annual_beta = NaN(ny, nx);
+CP_NEP_annual_r = NaN(ny, nx);
+CP_NEP_annual_p = NaN(ny, nx);
+CP_NEP_annual_beta = NaN(ny, nx);
+EP_NEP_shyear_r = NaN(ny, nx);
+EP_NEP_shyear_p = NaN(ny, nx);
+EP_NEP_shyear_beta = NaN(ny, nx);
+CP_NEP_shyear_r = NaN(ny, nx);
+CP_NEP_shyear_p = NaN(ny, nx);
+CP_NEP_shyear_beta = NaN(ny, nx);
+
+for i = 1:ny
+    for j = 1:nx
+        ts = squeeze(NEP_annual_mean(i,j,:));
+        if sum(isnan(ts))==0
+            [r,p] = corr(epi, ts, 'rows','pairwise');
+            mdl = fitlm(epi, ts);
+            EP_NEP_annual_r(i,j) = r;
+            EP_NEP_annual_p(i,j) = p;
+            EP_NEP_annual_beta(i,j) = mdl.Coefficients.Estimate(2);
+            
+            [r,p] = corr(cpi, ts, 'rows','pairwise');
+            mdl = fitlm(cpi, ts);
+            CP_NEP_annual_r(i,j) = r;
+            CP_NEP_annual_p(i,j) = p;
+            CP_NEP_annual_beta(i,j) = mdl.Coefficients.Estimate(2);
+        end
+        ts = squeeze(NEP_shyear_mean(i,j,:));
+        if sum(isnan(ts))==0
+            [r,p] = corr(epi, ts, 'rows','pairwise');
+            mdl = fitlm(epi, ts);
+            EP_NEP_shyear_r(i,j) = r;
+            EP_NEP_shyear_p(i,j) = p;
+            EP_NEP_shyear_beta(i,j) = mdl.Coefficients.Estimate(2);
+            
+            [r,p] = corr(cpi, ts, 'rows','pairwise');
+            mdl = fitlm(cpi, ts);
+            CP_NEP_shyear_r(i,j) = r;
+            CP_NEP_shyear_p(i,j) = p;
+            CP_NEP_shyear_beta(i,j) = mdl.Coefficients.Estimate(2);
+        end
+    end
+end
+clear i j mdl ts r p;
+
+
+%% Regress monthly & annual gpp against CPI and EPI indices
+load ../data/nep_inversions_regional.mat;
+clear temp *africa* *amazon* *austr* *casia* *eastus* *europe* *extratropical* *grass* *sahel* *semiarid* *tropical* *tundra* *westna*;
+load ../data/cpi_epi_1951-2016.mat;
+
+cpi = cpi(yr>=syear & yr<=eyear);
+epi = epi(yr>=syear & yr<=eyear);
+yr = yr(yr>=syear & yr<=eyear);
+
+NEP_tropics_annual = NEP_tropics_annual(years>=syear & years<=eyear, :);
+NEP_tropics_annual_mean = NEP_tropics_annual_mean(years>=syear & years<=eyear, :);
+NEP_tropics_shyear = NEP_tropics_shyear(years>=syear & years<=eyear, :);
+NEP_tropics_shyear_mean = NEP_tropics_shyear_mean(years>=syear & years<=eyear, :);
+NEP_tropics_monthly = NEP_tropics_monthly(years>=syear & years<=eyear, :, :);
+NEP_tropics_monthly_mean = NEP_tropics_monthly_mean(years>=syear & years<=eyear, :);
+years = years(years>=syear & years<=eyear)';
+
+EP_NEP_tropics_monthly_beta = NaN(12, length(models));
+EP_NEP_tropics_monthly_mean_beta = NaN(12, 1);
+EP_NEP_tropics_monthly_mean_beta_CI = NaN(12, 1);
+EP_NEP_tropics_annual_beta = NaN(1, length(models));
+EP_NEP_tropics_shyear_beta = NaN(1, length(models));
+
+mdl = fitlm(epi, NEP_tropics_annual_mean);
+EP_NEP_tropics_annual_mean_beta = mdl.Coefficients.Estimate(2);
+EP_NEP_tropics_annual_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Tropical inversion mean response to EPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+mdl = fitlm(epi, NEP_tropics_shyear_mean);
+EP_NEP_tropics_shyear_mean_beta = mdl.Coefficients.Estimate(2);
+EP_NEP_tropics_shyear_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Tropical inversion mean response to EPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+for i = 1:12
+    mdl = fitlm(epi, NEP_tropics_monthly_mean(:, i));
+    EP_NEP_tropics_monthly_mean_beta(i) = mdl.Coefficients.Estimate(2);
+    EP_NEP_tropics_monthly_mean_beta_CI(i) = 1.96*mdl.Coefficients.SE(2);
+    for j = 1:length(models)
+        mdl = fitlm(epi, NEP_tropics_monthly(:, i, j));
+        EP_NEP_tropics_monthly_beta(i, j) = mdl.Coefficients.Estimate(2);
+        if i == 1
+            mdl = fitlm(epi, NEP_tropics_annual(:, j));
+            EP_NEP_tropics_annual_beta(j) = mdl.Coefficients.Estimate(2);
+            mdl = fitlm(epi, NEP_tropics_shyear(:, j));
+            EP_NEP_tropics_shyear_beta(j) = mdl.Coefficients.Estimate(2);
+        end
+    end
+    
+end
+
+CP_NEP_tropics_monthly_beta = NaN(12, length(models));
+CP_NEP_tropics_monthly_mean_beta = NaN(12, 1);
+CP_NEP_tropics_monthly_mean_beta_CI = NaN(12, 1);
+CP_NEP_tropics_annual_beta = NaN(1, length(models));
+CP_NEP_tropics_shyear_beta = NaN(1, length(models));
+
+mdl = fitlm(cpi, NEP_tropics_annual_mean);
+CP_NEP_tropics_annual_mean_beta = mdl.Coefficients.Estimate(2);
+CP_NEP_tropics_annual_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Tropical inversion mean response to CPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+mdl = fitlm(cpi, NEP_tropics_shyear_mean);
+CP_NEP_tropics_shyear_mean_beta = mdl.Coefficients.Estimate(2);
+CP_NEP_tropics_shyear_mean_beta_CI = 1.96*mdl.Coefficients.SE(2);
+disp(['Tropical inversion mean response to CPI (PgC yr-1): ',...
+    num2str(round(mdl.Coefficients.Estimate(2)/1000,2)),' +/- ',...
+    num2str(round(1.96*mdl.Coefficients.SE(2)/1000,2))]);
+for i = 1:12
+    mdl = fitlm(cpi, NEP_tropics_monthly_mean(:, i));
+    CP_NEP_tropics_monthly_mean_beta(i) = mdl.Coefficients.Estimate(2);
+    CP_NEP_tropics_monthly_mean_beta_CI(i) = 1.96*mdl.Coefficients.SE(2);
+    for j = 1:length(models)
+        mdl = fitlm(cpi, NEP_tropics_monthly(:, i, j));
+        CP_NEP_tropics_monthly_beta(i, j) = mdl.Coefficients.Estimate(2);
+        if i == 1
+            mdl = fitlm(cpi, NEP_tropics_annual(:, j));
+            CP_NEP_tropics_annual_beta(j) = mdl.Coefficients.Estimate(2);
+            mdl = fitlm(cpi, NEP_tropics_shyear(:, j));
+            CP_NEP_tropics_shyear_beta(j) = mdl.Coefficients.Estimate(2);
+        end
+    end
+    
+end
+
+clear i j ts r p mdl nt nx ny scale syear NEP mo;
+
+save('./data/cp_ep_nep_inversions.mat');
+
